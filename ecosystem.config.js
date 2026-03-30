ec15db70692bb213272883cc11db92f4a1413c3e
harsha200210 <harshadilanbb2020@gmail.com>
2026-03-30 23:47:36 +0530

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
           name: "cloud-sql-auth-proxy",
-          script: "./cloud-sql-proxy its-2130-eca-hdse-69-70-489518:asia-southeast1:mysql-vm its-2130-eca-hdse-69-70-489518:asia-southeast1:postgres-vm --private-ip",
+          script: "./cloud-sql-proxy ecaproject-01:asia-southeast1:mysql-instance --private-ip",
           log_file: "./logs/cloud-sql-proxy.log",
         },
         {
